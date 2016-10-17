6b93e790bfe48027b67b784f76b236609d220d97
t00183089 <joshua.standing-harvey@students.ittralee.ie>
2016-10-17 12:39:59 +0100

    adding all of ricks guitar
    adding ricks guitar java classes

---
+++ Builder.java
@@ -0,0 +1,8 @@
+/**
+ * Created by t00183089 on 26/09/2016.
+ */
+public enum Builder{
+    GIBSON, FENDER, PRS, RYAN, COLLINGS, OLSON, MARTIN
+}
+
+
---
+++ Type.java
@@ -0,0 +1,6 @@
+/**
+ * Created by t00183089 on 26/09/2016.
+ */
+public enum Type {
+    ELECTRIC, ACOUSTIC
+}
